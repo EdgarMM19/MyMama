7fe12335599c3c08956e305cab1907d8123a0dd3
EdgarMM19 <edgarmm19@gmail.com>
2021-05-15 18:32:38 +0200

    Cositas se vienen

--- my_mama/lib/callMyMamaPage.dart
+++ my_mama/lib/callMyMamaPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_mama/calendarPage.dart';
 import 'todoList.dart';
+import 'package:multi_select_item/multi_select_item.dart';
+import 'connection.dart';
 
 class CallMyMamaPage extends StatefulWidget {
   const CallMyMamaPage({Key key}) : super(key: key);
@@ -10,13 +12,94 @@ class CallMyMamaPage extends StatefulWidget {
 }
 
 class _CallMyMamaPageState extends State<CallMyMamaPage> {
+  List mainList = new List();
+  MultiSelectController controller = new MultiSelectController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    mainList.add(Task("Gym", 10, 250, "Sport"));
+    mainList.add(Task("Run", 60, 250, "Sport"));
+    mainList.add(Task("Swim", 120, 250, "Sport"));
+    mainList.add(Task("Dinner with friends", 180, 1200, "Social"));
+
+    controller.disableEditingWhenNoneSelected = true;
+    controller.set(mainList.length);
+  }
+
+  void send() {
+    // mainList[controller.selectedIndexes] to send to myMama
+  }
+
+  void delete() {
+    var list = controller.selectedIndexes;
+    list.sort((b, a) =>
+        a.compareTo(b)); //reoder from biggest number, so it wont error
+    list.forEach((element) {
+      mainList.removeAt(element);
+    });
+
+    setState(() {
+      controller.set(mainList.length);
+    });
+  }
+
+  void selectAll() {
+    setState(() {
+      controller.toggleAll();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      child: Text("PRESS HERE"),
-      onPressed: () {
-        print("Mhan pitjat");
+    return WillPopScope(
+      onWillPop: () async {
+        //block app from quitting when selecting
+        var before = !controller.isSelecting;
+        setState(() {
+          controller.deselectAll();
+        });
+        return before;
       },
+      child: new Scaffold(
+        appBar: new AppBar(
+          title:
+              new Text('Selected ${controller.selectedIndexes.length} tasks'),
+          backgroundColor: Colors.teal,
+        ),
+        body: ListView.builder(
+          itemCount: mainList.length,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {},
+              child: MultiSelectItem(
+                isSelecting: controller.isSelecting,
+                onSelected: () {
+                  setState(() {
+                    controller.toggle(index);
+                  });
+                },
+                child: Container(
+                  child: ListTile(
+                    title: new Text("${mainList[index]['name']}"),
+                    subtitle: new Text(
+                        "${mainList[index]['type']} for ${mainList[index]['span']}"),
+                  ),
+                  decoration: controller.isSelected(index)
+                      ? new BoxDecoration(color: Colors.grey[300])
+                      : new BoxDecoration(),
+                ),
+              ),
+            );
+          },
+        ),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: send,
+          tooltip: 'Increment',
+          child: Text("Call myMama"),
+        ),
+      ),
     );
   }
 }
