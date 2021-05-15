abeea44660f09c05123a1a4d004110b24d695d23
asleix <aleixsegui12@gmail.com>
2021-05-15 16:59:24 +0200

    updated connections

--- scheduler/test_connection.py
+++ scheduler/test_connection.py
@@ -1,6 +1,14 @@
 import requests
+import json
+
+testdict = {'a':1, 'b':2}
+testjson = json.dumps(testdict)
+
+
 
 
 URL = "http://localhost:8080"
 r = requests.get(url = URL)
 print(r.json())
+requests.post(url=URL, data=testdict)
+requests.post(url=URL, data=testjson)
