b1eb359f218050ef7b7a6184cb8991cf61fc4292
asleix <aleixsegui12@gmail.com>
2021-05-15 18:29:43 +0200

    Added POST http

--- my_mama/lib/connection.dart
+++ my_mama/lib/connection.dart
@@ -20,18 +20,41 @@ class Task {
     this.start = json['start_time'] as int;
     this.type = json['type'] as String;
   }
+
+  Map<String, dynamic> toJson() =>
+      {'name': name, 'span': span, 'start_time': start, 'type': type};
 }
 
 // A function that converts a response body into a List<Photo>.
-List<Task> parsePhotos(String responseBody) {
+List<Task> parseTasks(String responseBody) {
   final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
 
   return parsed.map<Task>((json) => Task.fromJson(json)).toList();
 }
 
-Future<List<Task>> fetchPhotos(http.Client client) async {
+Future<List<Task>> fetchTasks(http.Client client) async {
   final response = await client.get(Uri.parse('http://localhost:8080'));
 
   // Use the compute function to run parsePhotos in a separate isolate.
-  return parsePhotos(response.body);
+  return parseTasks(response.body);
+}
+
+Map<String, dynamic> getJSON(List<Task> tasklist) {
+  Map<String, dynamic> taskdict;
+  int count = 0;
+  for (Task t in tasklist) {
+    taskdict['task' + count.toString()] = t.toJson();
+    count = count + 1;
+  }
+  return taskdict;
+}
+
+Future<http.Response> sendTasks(List<Task> tasklist) {
+  return http.post(
+    Uri.https('jsonplaceholder.typicode.com', ''),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(getJSON(tasklist)),
+  );
 }
