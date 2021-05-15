9cfde835f383d2c067a61af567495c42aca22223
alvaro-ribot <ribot.arb@gmail.com>
2021-05-15 12:50:32 +0200

    greedy done

--- scheduler/classes.py
+++ scheduler/classes.py
@@ -1,17 +1,12 @@
 #def parameters
-
-class Type:
-    def __init__()
-
-
 class To_do:
-    def __init__(self, name, size, type):
+    def __init__(self, name, size, type = "undef"):
         self.name = name
         self.size = size
         self.type = type
 
 class Event:
-    def __init__(name, time_start, time_end, type, fixed=False):
+    def __init__(self, name, time_start, time_end, type="undef", fixed=False):
         self.name = name
         self.time_start = time_start
         self.time_end = time_end
--- scheduler/greedy_schedule.py
+++ scheduler/greedy_schedule.py
@@ -1,13 +1,28 @@
+import numpy as np
+from classes import *
 #rest = vector de restriccions,
-#tasks= vector de size de to-do's
+#tasks= vector de to-do's
 #start, end = start/end of day
 def greedy_schedule(tasks, rest, start=8*60, end=22*60):
-    tasks.sort(reverse = True)
-    rest.extend([(0,start), (end,24*60)])
-    rest.sort()
+    tasks.sort(reverse = True, key = lambda x: x.size)
+    rest.extend([Event("dormir",0,start), Event("dormir", end,24*60)])
+    rest.sort(key = lambda x: x.time_start)
+    schedule = dict()
 
     for t in tasks:
         for i in range(len(rest)-1):
-            if rest[i][1] + t <= rest[i+1][0]:
-                rest.insert(i+1, (rest[i][1], rest[i][1] + t))
+            if rest[i].time_end + t.size <= rest[i+1].time_start:
+                rest.insert(i+1, Event(t.name, rest[i].time_end, rest[i].time_end + t.size))
+                schedule[t.name] = (rest[i].time_end, rest[i].time_end + t.size)
+                #schedule[t.name] = (rest[i].time_end/60, (rest[i].time_end + t.size)/60)
                 break
+
+    return(schedule)
+
+
+tasks = [To_do("correr",30), To_do("deures",60), To_do("churri",120), To_do("compra",45), To_do("cuinar",60)]
+rest = [Event("clase",9*60,11*60), Event("clase",12*60, 14*60), Event("clase",16*60,17*60)]
+#rest = [(x[0]*60,x[1]*60) for x in clases]
+
+result = greedy_schedule(tasks, rest)
+print(result)
