ea9970a2ea776e5b2c4cee53c63437cabc43eb92
EdgarMM19 <edgarmm19@gmail.com>
2021-05-15 11:54:02 +0200

    Lista mejor

--- my_mama/lib/main.dart
+++ my_mama/lib/main.dart
@@ -44,7 +44,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 children: [
                   Text("MyMamma",
                       style: TextStyle(
-                          fontSize: 72.0, fontWeight: FontWeight.bold))
+                          fontSize: 72.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal)),
                 ],
               ),
             ),
@@ -69,18 +71,19 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
-  final List<ListItem> items = List<ListItem>.generate(
+  final List<TodoItem> items = List<TodoItem>.generate(
     1000,
-    (i) => i % 6 == 0
-        ? HeadingItem('Heading $i')
-        : MessageItem('Sender $i', 'Message body $i'),
+    (i) => TodoItem(
+      itemData: i.toString(),
+      itemHour: i.toString(),
+    ),
   );
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 500.0,
         width: 500.0,
+        height: 500.0,
         child: ListView.builder(
           shrinkWrap: true,
           scrollDirection: Axis.vertical,
@@ -90,51 +93,42 @@ class _TodoListState extends State<TodoList> {
           // Convert each item into a widget based on the type of item it is.
           itemBuilder: (context, index) {
             final item = items[index];
-
-            return ListTile(
-              title: item.buildTitle(context),
-              subtitle: item.buildSubtitle(context),
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: item,
             );
           },
         ));
   }
 }
 
-abstract class ListItem {
-  /// The title line to show in a list item.
-  Widget buildTitle(BuildContext context);
-
-  /// The subtitle line, if any, to show in a list item.
-  Widget buildSubtitle(BuildContext context);
-}
-
-class HeadingItem implements ListItem {
-  final String heading;
-
-  HeadingItem(this.heading);
-
+class TodoItem extends StatelessWidget {
+  final dynamic itemData;
+  final dynamic itemHour;
+  const TodoItem({Key key, @required this.itemData, this.itemHour})
+      : super(key: key);
   @override
-  Widget buildTitle(BuildContext context) {
-    return Text(
-      heading,
-      style: Theme.of(context).textTheme.headline5,
-    );
+  Widget build(BuildContext context) {
+    return Container(
+        width: 1000.0,
+        decoration: BoxDecoration(
+            color: Colors.amber,
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: Colors.black, width: 1)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                child: Text(itemHour,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.teal))),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
+                child: Text(itemData)),
+          ],
+        ));
   }
-
-  @override
-  Widget buildSubtitle(BuildContext context) => SizedBox();
-}
-
-/// A ListItem that contains data to display a message.
-class MessageItem implements ListItem {
-  final String sender;
-  final String body;
-
-  MessageItem(this.sender, this.body);
-
-  @override
-  Widget buildTitle(BuildContext context) => Text(sender);
-
-  @override
-  Widget buildSubtitle(BuildContext context) => Text(body);
 }
