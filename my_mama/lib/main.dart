747fcfb1489f3eb6b66e0032a2ef27534ccf7fb1
asleix <aleixsegui12@gmail.com>
2021-05-16 03:14:34 +0200

    Update main.dart

--- my_mama/lib/main.dart
+++ my_mama/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xFF2196f3),
         accentColor: const Color(0xFF2196f3),
         canvasColor: const Color(0xFFfafafa),
+        backgroundColor: const Color(0xFFFAF0),
       ),
       home: new MyHomePage(),
     );
@@ -42,28 +43,42 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _prefs.then((SharedPreferences prefs) {
         prefs.clear();
-        prefs.setString("data", jsonEncode(
-            [
-              ConfigActivityOneTime(name: "Restaurant", span: 60, genre: "Meal", whenDia: 6, whenMinut: 14*60),
-              ConfigFixedActivityFreeHour(name: "Running", span: 60, genre: "Sport", whenDia: [0, 3, 6]),
-              ConfigFixedActivity(name: "Real", span: 60, genre: "Lectures", whenDia: [0, 3, 5, 6], whenMinut: [12*60, 13*60, 23*60 + 55, 9*60]),
-              ConfigActivityFreeHour(name: "Rentadora", span: 30, genre: "Hygiene")
-            ]
-        ));
+        prefs.setString(
+            "data",
+            jsonEncode([
+              ConfigActivityOneTime(
+                  name: "Restaurant",
+                  span: 60,
+                  genre: "Meal",
+                  whenDia: 6,
+                  whenMinut: 14 * 60),
+              ConfigFixedActivityFreeHour(
+                  name: "Running",
+                  span: 60,
+                  genre: "Sport",
+                  whenDia: [0, 3, 6]),
+              ConfigFixedActivity(
+                  name: "Real",
+                  span: 60,
+                  genre: "Lectures",
+                  whenDia: [0, 3, 5, 6],
+                  whenMinut: [12 * 60, 13 * 60, 23 * 60 + 55, 9 * 60]),
+              ConfigActivityFreeHour(
+                  name: "Rentadora", span: 30, genre: "Hygiene")
+            ]));
       });
     });
   }
 
   Future<List<ConfigActivity>> consultaConfigs() {
     return _prefs.then((SharedPreferences pref) {
-      List<ConfigActivity> listConfig = jsonDecode(pref.getString("data")).map<ConfigActivity>(
-              (e) {
-                if (e["type"] == 1) return ConfigFixedActivity.fromJson(e);
-                if (e["type"] == 2) return ConfigFixedActivityFreeHour.fromJson(e);
-                if (e["type"] == 3) return ConfigActivityFreeHour.fromJson(e);
-                if (e["type"] == 4) return ConfigActivityOneTime.fromJson(e);
-              }
-      ).toList();
+      List<ConfigActivity> listConfig =
+          jsonDecode(pref.getString("data")).map<ConfigActivity>((e) {
+        if (e["type"] == 1) return ConfigFixedActivity.fromJson(e);
+        if (e["type"] == 2) return ConfigFixedActivityFreeHour.fromJson(e);
+        if (e["type"] == 3) return ConfigActivityFreeHour.fromJson(e);
+        if (e["type"] == 4) return ConfigActivityOneTime.fromJson(e);
+      }).toList();
       return listConfig;
     });
   }
@@ -73,9 +88,8 @@ class _MyHomePageState extends State<MyHomePage> {
     data.add(newConfig);
     setState(() {
       _prefs.then((SharedPreferences pref) {
-        pref.setString("data",
-            jsonEncode(data.map((e) => e.toJson()).toList())
-        );
+        pref.setString(
+            "data", jsonEncode(data.map((e) => e.toJson()).toList()));
       });
     });
   }
@@ -85,28 +99,23 @@ class _MyHomePageState extends State<MyHomePage> {
     data.remove(delConfig);
     setState(() {
       _prefs.then((SharedPreferences pref) {
-        pref.setString("data",
-            jsonEncode(data.map((e) => e.toJson()).toList())
-        );
+        pref.setString(
+            "data", jsonEncode(data.map((e) => e.toJson()).toList()));
       });
     });
   }
-
 
   void saveConfigs(List<ConfigActivity> configs) async {
     setState(() {
       _prefs.then((SharedPreferences pref) {
-        pref.setString("data",
-            jsonEncode(configs.map((e) => e.toJson()).toList())
-        );
+        pref.setString(
+            "data", jsonEncode(configs.map((e) => e.toJson()).toList()));
       });
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-
     Map<String, Function> dataQueries = {
       "addConfig": addConfig,
       "consultaConfigs": consultaConfigs,
@@ -117,15 +126,13 @@ class _MyHomePageState extends State<MyHomePage> {
     final PageController controller = PageController(initialPage: 1);
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: PageView(
-        scrollDirection: Axis.horizontal,
-        controller: controller,
-        children: <Widget> [
+        body: PageView(
+            scrollDirection: Axis.horizontal,
+            controller: controller,
+            children: <Widget>[
           ConfigurationPage(dataQueries: dataQueries),
           CalendarPage(dataQueries: dataQueries),
           CallMyMamaPage(dataQueries: dataQueries),
-        ]
-      )
-    );
+        ]));
   }
 }
